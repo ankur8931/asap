0658b455e694b0349626d372e05e2b359b75e87c
ankur8931 <achaud16@asu.edu>
2020-08-03 04:32:05 -0700

    adding Stinger source code

---
+++ PortScan.py
@@ -0,0 +1,28 @@
+import nmap
+
+class PortScan:
+  
+   def __init__(self):
+
+      print("Scanning Network")
+
+   def scanTarget(self, ip):
+
+      nm = nmap.PortScanner()
+      nm.scan(hosts=ip)
+      serv_list = list()
+
+      for proto in nm[ip].all_protocols():
+          lport = nm[ip][proto].keys()
+
+          for port in lport:
+
+              serv_list.append({"port":port,"state":nm[ip][proto][port]['state'],"protocol":proto,"service_name":nm[ip][proto][port]['product'], "cpe":nm[ip][proto][port]['cpe']})
+      print(serv_list)
+
+      return serv_list
+
+if __name__ == "__main__":
+   ps = PortScan()
+   ps.scanTarget('192.168.3.29')
+
